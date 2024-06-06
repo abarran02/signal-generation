94ab58298880b2985004ab55ba022249f8936786
stacey.lei@berkeley.edu <stacey.232110@gmail.com>
2024-06-06 14:37:15 -0400

    add PW and LFM routes

--- views/schema.py
+++ views/schema.py
@@ -8,3 +8,20 @@ class CWSchema(Schema):
     num_reps = fields.Integer(required=True)
     sample_rate = fields.Integer(required=True)
     form = fields.String(required=True)
+
+class PWSchema(Schema):
+    sample_rate = fields.Integer(required=True)
+    bit_length = fields.Float(required=True)
+    num_bits = fields.Integer(required=True)
+    taps = fields.List(fields.Nested(fields.Integer), required=True)
+    amplitude = fields.Integer(required=True)
+    pri = fields.Float(required=True)
+    num_pulses_pw = fields.Integer(required=True)
+
+class LFMSchema(Schema):
+    pulse_width_lfm = fields.Float(required=True)
+    pri = fields.Float(required=True)
+    num_reps = fields.Integer(required=True)
+    fstart = fields.Integer(required=True)
+    fstop = fields.Integer(required=True)
+    sample_rate_lfm = fields.Integer(required=True)
--- views/views.py
+++ views/views.py
@@ -5,9 +5,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 from flask import Blueprint, request, send_file
 from marshmallow import ValidationError
+from numpy.typing import NDArray
 
 import signal_utils as su
 from signal_utils.common.binary_file_ops import get_iq_bytes
+from signal_utils.common.sequences import maximal_length_sequence
 
 from .schema import *
 
@@ -23,34 +25,69 @@ def get_cw():
 
         if data["form"] == "sc16":
             pulse_bytes = get_iq_bytes(pulse)
-
-            # get current time for file naming
-            now = datetime.now()
-            formatted_time = now.strftime("%Y%m%d_%H%M%S")
-
-            return send_file(
-                BytesIO(pulse_bytes),
-                mimetype="application/octet-stream",
-                as_attachment=True,
-                download_name=f"cw_{formatted_time}.sc16"
-            )
+            return send_bytes_response(pulse_bytes, "cw")
 
         elif data["form"] == "png":
             t = np.linspace(0, data["pulse_width"], pulse.shape[0])
-            buf = BytesIO()
-            plt.figure()
-            plt.plot(t, np.real(pulse), label="In-phase (I)")
-            plt.plot(t, np.imag(pulse), label="Quadrature (Q)", linestyle="--")
-            plt.title("Radar Signal in Time Domain")
-            plt.xlabel("Time (s)")
-            plt.ylabel("Amplitude")
-
-            plt.savefig(buf, format="png")
-            buf.seek(0)
-            return send_file(
-                buf,
-                mimetype="image/png"
-            )
+            return send_plot_image(pulse, t)
 
     except ValidationError as err:
         return {"errors": err.messages}, 400
+
+@wave_views.route("/pw", methods=["GET"])
+def get_pw():
+    schema = PWSchema()
+
+    try:
+        data = schema.load(request.args)
+        seq = maximal_length_sequence(data["num_bits"], np.array(data["taps"]))
+        pulse_seq = su.radar_pulse.generate_pulse(seq, data["sample_rate"], data["bit_length"], data["pri"], data["num_pulses"])
+        pulse_seq = np.round(data["amplitude"] * pulse_seq)
+        pulse_bytes = get_iq_bytes(pulse_seq)
+
+        return send_bytes_response(pulse_bytes, "pw")
+
+    except ValidationError as err:
+        return {"errors": err.messages}, 400
+
+@wave_views.route("/lfm", methods=["GET"])
+def get_lfm():
+    schema = LFMSchema()
+    try:
+        data = schema.load(request.args)
+        pulse = su.generate_lfm(data["sample_rate_lfm"], data["f_start"], data['f_stop'], data["signal_length"]) #running into a module error here... check out
+        pulse_bytes = get_iq_bytes(pulse)
+
+        return send_bytes_response(pulse_bytes)
+
+    except ValidationError as err:
+        return {"errors": err.messages}, 400
+
+def send_bytes_response(pulse_bytes: bytes, prefix: str):
+    # get current time for file naming
+    now = datetime.now()
+    formatted_time = now.strftime("%Y%m%d_%H%M%S")
+
+    return send_file(
+        BytesIO(pulse_bytes),
+        mimetype="application/octet-stream",
+        as_attachment=True,
+        download_name=f"{prefix}_{formatted_time}.sc16"
+    )
+
+def send_plot_image(pulse: NDArray[np.complex_], t: NDArray[np.float_]):
+    buf = BytesIO()
+    plt.figure()
+    plt.plot(t, np.real(pulse), label="In-phase (I)")
+    plt.plot(t, np.imag(pulse), label="Quadrature (Q)", linestyle="--")
+    plt.title("Radar Signal in Time Domain")
+    plt.xlabel("Time (s)")
+    plt.ylabel("Amplitude")
+    plt.legend()
+
+    plt.savefig(buf, format="png")
+    buf.seek(0)
+    return send_file(
+        buf,
+        mimetype="image/png"
+    )
