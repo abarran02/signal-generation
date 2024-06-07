4c2779674122eed5dee3bc4cca31901ec1f9526b
Alec Barran <alexanderbarran@gmail.com>
2024-06-07 13:18:25 -0400

    add Plotly interactive graph page and organize response types

---
+++ views/response.py
@@ -0,0 +1,66 @@
+from datetime import datetime
+from io import BytesIO
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import plotly.express as px
+from flask import Response, render_template, send_file
+from numpy.typing import NDArray
+
+from signal_utils.common.binary_file_ops import get_iq_bytes
+
+
+def send_bytes_response(pulse_bytes: bytes, prefix: str):
+    # get current time for file naming
+    now = datetime.now()
+    formatted_time = now.strftime("%Y%m%d_%H%M%S")
+
+    return send_file(
+        BytesIO(pulse_bytes),
+        mimetype="application/octet-stream",
+        as_attachment=True,
+        download_name=f"{prefix}_{formatted_time}.sc16"
+    )
+
+def send_interactive_graph(pulse: NDArray[np.complex_], t: NDArray[np.float_], abbr: str):
+    df = pd.DataFrame({"real": np.real(pulse), "imag": np.imag(pulse)})
+    fig = px.line(df,
+        x=t,
+        y=df.columns,
+        title=f"{abbr.upper()} Graph"
+    )
+    fig.update_layout(xaxis_title="Time (s)", yaxis_title="Amplitude")
+    fig_html = fig.to_html()
+
+    return render_template("graph.html", fig_html=fig_html, title=f"{abbr.upper()} Graph")
+
+def send_plot_image(pulse: NDArray[np.complex_], t: NDArray[np.float_]):
+    buf = BytesIO()
+    plt.figure()
+    plt.plot(t, np.real(pulse), label="In-phase (I)")
+    plt.plot(t, np.imag(pulse), label="Quadrature (Q)", linestyle="--")
+    plt.title("Radar Signal in Time Domain")
+    plt.xlabel("Time (s)")
+    plt.ylabel("Amplitude")
+    plt.legend()
+
+    plt.savefig(buf, format="png")
+    buf.seek(0)
+    return send_file(
+        buf,
+        mimetype="image/png"
+    )
+
+def output_cases(pulse: NDArray[np.complex_], form: str, tstop: float, abbr: str) -> Response:
+    if form == "sc16":
+        pulse_bytes = get_iq_bytes(pulse)
+        return send_bytes_response(pulse_bytes, abbr)
+
+    elif form == "png":
+        t = np.linspace(0, tstop, pulse.shape[0])
+        return send_plot_image(pulse, t)
+
+    elif form == "graph":
+        t = np.linspace(0, tstop, pulse.shape[0])
+        return send_interactive_graph(pulse, t, abbr)
--- views/views.py
+++ views/views.py
@@ -1,17 +1,11 @@
-from datetime import datetime
-from io import BytesIO
-
 import numpy as np
-import pandas as pd
-import plotly.express as px
-from flask import Blueprint, request, send_file, jsonify
+from flask import Blueprint, request
 from marshmallow import ValidationError
-from numpy.typing import NDArray
 
 import signal_utils as su
-from signal_utils.common.binary_file_ops import get_iq_bytes
 from signal_utils.common.sequences import maximal_length_sequence
 
+from .response import output_cases
 from .schema import *
 
 wave_views = Blueprint("wave_views", __name__, url_prefix="/generate")
@@ -24,13 +18,7 @@ def get_cw():
         data = schema.load(request.args)
         pulse = su.continuous_wave.generate_cw(data["sample_rate"], data["signal_length"])
 
-        if data["form"] == "sc16":
-            pulse_bytes = get_iq_bytes(pulse)
-            return send_bytes_response(pulse_bytes, "cw")
-
-        elif data["form"] == "graph":
-            t = np.linspace(0, data["signal_length"], pulse.shape[0])
-            return send_plot_image(pulse, t)
+        return output_cases(pulse, data["form"], data["signal_length"], "CW")
 
     except ValidationError as err:
         return {"errors": err.messages}, 400
@@ -45,13 +33,7 @@ def get_pw():
         pulse = su.radar_pulse.generate_pulse(seq, data["sample_rate"], data["bit_length"], data["pri"], data["num_pulses"])
         pulse = np.round(data["amplitude"] * pulse)
 
-        if data["form"] == "sc16":
-            pulse_bytes = get_iq_bytes(pulse)
-            return send_bytes_response(pulse_bytes, "pw")
-
-        elif data["form"] == "graph":
-            t = np.linspace(0, data["bit_length"], pulse.shape[0])
-            return send_plot_image(pulse, t)
+        return output_cases(pulse, data["form"], data["bit_length"], "PW")
 
     except ValidationError as err:
         return {"errors": err.messages}, 400
@@ -64,32 +46,7 @@ def get_lfm():
         data = schema.load(request.args)
         pulse = su.linear_frequency_modulated.generate_lfm(data["sample_rate"], data["fstart"], data['fstop'], data["signal_length"])
 
-        if data["form"] == "sc16":
-            pulse_bytes = get_iq_bytes(pulse)
-            return send_bytes_response(pulse_bytes, "lfm")
-
-        elif data["form"] == "graph":
-            t = np.linspace(0, data["signal_length"], pulse.shape[0])
-            return send_plot_image(pulse, t)
+        return output_cases(pulse, data["form"], data["signal_length"], "lfm")
 
     except ValidationError as err:
         return {"errors": err.messages}, 400
-
-def send_bytes_response(pulse_bytes: bytes, prefix: str):
-    # get current time for file naming
-    now = datetime.now()
-    formatted_time = now.strftime("%Y%m%d_%H%M%S")
-
-    return send_file(
-        BytesIO(pulse_bytes),
-        mimetype="application/octet-stream",
-        as_attachment=True,
-        download_name=f"{prefix}_{formatted_time}.sc16"
-    )
-
-def send_plot_image(pulse: NDArray[np.complex_], t: NDArray[np.float_]):
-    df = pd.DataFrame({"real": np.real(pulse), "imag": np.imag(pulse)})
-    fig = px.line(df, x=t, y=df.columns, labels={'x':'Time (s)', 'y':'Amplitude'})
-    fig_html = fig.to_html(full_html=False, include_plotlyjs=False)
-
-    return jsonify(fig_html)
