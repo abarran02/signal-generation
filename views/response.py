084b976afab2da54f86b8a96bc7c38d0cc7cd44a
Alec Barran <alexanderbarran@gmail.com>
2024-06-10 13:56:57 -0400

    use AGG backend for matplotlib to fix random crashes

--- views/response.py
+++ views/response.py
@@ -1,17 +1,18 @@
 from datetime import datetime
 from io import BytesIO
 
+import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import plotly.express as px
 from flask import Response, render_template, send_file
 from numpy.typing import NDArray
-import plotly.express as px
-
 
 from signal_utils.common.binary_file_ops import get_iq_bytes
 
+# limit matplotlib to png backend
+matplotlib.use("agg")
 
 def send_bytes_response(pulse_bytes: bytes, prefix: str):
     # get current time for file naming
@@ -38,7 +39,6 @@ def send_interactive_graph(pulse: NDArray[np.complex_], t: NDArray[np.float_], a
     return render_template("graph.jinja", fig_html=fig_html, title=f"{abbr.upper()} Graph")
 
 def send_plot_image(pulse: NDArray[np.complex_], t: NDArray[np.float_], abbr: str):
-    buf = BytesIO()
     plt.plot(t, np.real(pulse), label="In-phase (I)")
     plt.plot(t, np.imag(pulse), label="Quadrature (Q)", linestyle="--")
     plt.title(f"{abbr.upper()} Signal in Time Domain")
@@ -46,24 +46,30 @@ def send_plot_image(pulse: NDArray[np.complex_], t: NDArray[np.float_], abbr: st
     plt.ylabel("Amplitude")
     plt.legend()
 
+    buf = BytesIO()
     plt.savefig(buf, format="png")
+    plt.clf()
+
     buf.seek(0)
     return send_file(
         buf,
         mimetype="image/png"
     )
+
 def create_three_dim_graph(pulse: NDArray[np.complex_], t: NDArray[np.float_], abbr: str):
-    df = pd.DataFrame({"real": np.real(pulse), "imag": np.imag(pulse)}) #valueError has something to do with scatter_3d
-    print(df.head())
-    fig = px.scatter_3d(df,
-                        x = df.loc[:, "real"],
-                        y = df.loc[:, "imag"],
-                        z = t,
-                        title = "3D Representation of " + abbr.upper()
-                        )
-                        
+    df = pd.DataFrame({"real": np.real(pulse), "imag": np.imag(pulse)})
+
+    fig = px.scatter_3d(
+        df,
+        x = df.loc[:, "real"],
+        y = df.loc[:, "imag"],
+        z = t,
+        title = "3D Representation of " + abbr.upper()
+    )
+
     fig.update_layout(height =800)
     fig_html = fig.to_html()
+
     return render_template("graph.jinja", fig_html=fig_html, title=f"{abbr.upper()} Graph")
 
 def output_cases(pulse: NDArray[np.complex_], form: str, tstop: float, abbr: str) -> Response:
@@ -78,6 +84,7 @@ def output_cases(pulse: NDArray[np.complex_], form: str, tstop: float, abbr: str
     elif form == "graph":
         t = np.linspace(0, tstop, pulse.shape[0])
         return send_interactive_graph(pulse, t, abbr)
+
     elif form == "threeDim":
         t = np.linspace(0, tstop, pulse.shape[0])
         return create_three_dim_graph(pulse, t, abbr)
