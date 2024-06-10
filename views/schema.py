11eba25f2189d666a478c3a1083a5eaa40ec26e7
Alec Barran <alexanderbarran@gmail.com>
2024-06-10 13:58:02 -0400

    enable using scientific notation in web interface fields with custom marshmallow Field

--- views/schema.py
+++ views/schema.py
@@ -1,28 +1,58 @@
-from marshmallow import Schema, fields
+from marshmallow import Schema, ValidationError, fields
+
+
+class ScientificNotation(fields.Field):
+    """Field that serializes to a string and deserializes
+    to a floating point number.
+
+    :param value_type: Specifies whether the field handles 'int' or 'float'. Defaults to 'int'.
+    :type value_type: str, optional
+    :param kwargs: Additional keyword arguments passed to the base Field class.
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.value_type = kwargs.get("value_type", "float")
+
+    def _serialize(self, value, attr, obj, **kwargs):
+        if value is None:
+            return ""
+
+        return str(value)
+
+    def _deserialize(self, value, attr, data, **kwargs):
+        try:
+            if self.value_type == "int":
+                # explicit cast to float because int(1e6) is invalid
+                return int(float(value))
+
+            return float(value)
+
+        except ValueError as error:
+            raise ValidationError("Invalid conversion to scientific notation.") from error
 
 class CWSchema(Schema):
-    sample_rate = fields.Integer(required=True)
-    signal_length = fields.Float(required=True)
+    sample_rate = ScientificNotation(value_type="int", required=True)
+    signal_length = ScientificNotation(required=True)
     form = fields.String(required=True)
 
 class RadarSchema(Schema):
-    sample_rate = fields.Integer(required=True)
-    bit_length = fields.Float(required=True)
-    num_bits = fields.Integer(required=True)
-    amplitude = fields.Integer(required=True)
-    pri = fields.Float(required=True)
-    num_pulses = fields.Integer(required=True)
+    sample_rate = ScientificNotation(value_type="int", required=True)
+    bit_length = ScientificNotation(required=True)
+    num_bits = ScientificNotation(value_type="int", required=True)
+    amplitude = ScientificNotation(value_type="int", required=True)
+    pri = ScientificNotation(required=True)
+    num_pulses = ScientificNotation(value_type="int", required=True)
     form = fields.String(required=True)
 
 class LFMSchema(Schema):
-    sample_rate = fields.Integer(required=True)
-    fstart = fields.Float(required=True)
-    fstop = fields.Float(required=True)
-    signal_length = fields.Float(required=True)
+    sample_rate = ScientificNotation(value_type="int", required=True)
+    fstart = ScientificNotation(required=True)
+    fstop = ScientificNotation(required=True)
+    signal_length = ScientificNotation(required=True)
     form = fields.String(required=True)
 
 class BPSKSchema(Schema):
-    sample_rate = fields.Integer(required=True)
-    bit_length = fields.Float(required=True)
-    num_bits = fields.Integer(required=True)
+    sample_rate = ScientificNotation(value_type="int", required=True)
+    bit_length = ScientificNotation(required=True)
+    num_bits = ScientificNotation(value_type="int", required=True)
     form = fields.String(required=True)
